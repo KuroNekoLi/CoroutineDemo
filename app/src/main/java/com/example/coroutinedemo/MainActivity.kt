848f18d0d96d4162d1686996714b8596f040e113
KuroNekoLi <127180158+KuroNekoLi@users.noreply.github.com>
2023-09-17 09:48:24 +0800

    switch the thread of a coroutine.

--- app/src/main/java/com/example/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/example/coroutinedemo/MainActivity.kt
@@ -7,13 +7,16 @@ import android.widget.Button
 import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
     private lateinit var btnDownloadUserData : Button
     private lateinit var btnCount : Button
     private lateinit var tvCount : TextView
+    private lateinit var tvUserMessage : TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,20 +25,27 @@ class MainActivity : AppCompatActivity() {
         btnDownloadUserData = findViewById(R.id.btnDownloadUserData)
         btnCount = findViewById(R.id.btnCount)
         tvCount = findViewById(R.id.tvCount)
+        tvUserMessage = findViewById(R.id.tvUserMessage)
 
         btnCount.setOnClickListener {
             tvCount.text = count++.toString()
         }
         btnDownloadUserData.setOnClickListener {
+            //launch 返回的是Job的實例，可以拿來指涉該協程，當我們沒有要返回值時使用
+            //如果要返回值，要用async，要調用await()得到這個值
             CoroutineScope(Dispatchers.IO).launch {
                 downloadUserData()
             }
         }
     }
 
-    private fun downloadUserData() {
+    suspend fun  downloadUserData() {
         for (i in 1..200000) {
-            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){
+                tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+
+            }
+//            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
         }
     }
 }
