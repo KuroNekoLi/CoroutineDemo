85846ce3751cfc3d91cd516d3e301d35943ea7fa
KuroNekoLi <127180158+KuroNekoLi@users.noreply.github.com>
2023-09-17 21:44:34 +0800

    async&await

--- app/src/main/java/com/example/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/example/coroutinedemo/MainActivity.kt
@@ -5,47 +5,48 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    private var count = 0
-    private lateinit var btnDownloadUserData : Button
-    private lateinit var btnCount : Button
-    private lateinit var tvCount : TextView
-    private lateinit var tvUserMessage : TextView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnDownloadUserData = findViewById(R.id.btnDownloadUserData)
-        btnCount = findViewById(R.id.btnCount)
-        tvCount = findViewById(R.id.tvCount)
-        tvUserMessage = findViewById(R.id.tvUserMessage)
 
-        btnCount.setOnClickListener {
-            tvCount.text = count++.toString()
-        }
-        btnDownloadUserData.setOnClickListener {
-            //launch 返回的是Job的實例，可以拿來指涉該協程，當我們沒有要返回值時使用
-            //如果要返回值，要用async，要調用await()得到這個值
-            CoroutineScope(Dispatchers.IO).launch {
-                downloadUserData()
-            }
+//        CoroutineScope(Dispatchers.IO).launch{
+//            Log.i("LinLi", "Calculation started: ");
+//            val stock1 = async { getStock1() }
+//            val stock2 = async { getStock2() }
+//            //調用兩個async的返回值
+//            val total = stock1.await()+stock2.await()
+//            Log.i("LinLi", "Total is: $total");
+//        }
+
+        CoroutineScope(Dispatchers.Main).launch{
+            Log.i("LinLi", "Calculation started: ");
+            val stock1 = async(Dispatchers.IO) { getStock1() }
+            val stock2 = async(Dispatchers.IO) { getStock2() }
+            //調用兩個async的返回值
+            val total = stock1.await()+stock2.await()
+            Toast.makeText(applicationContext,"Total is: $total", Toast.LENGTH_LONG)
+                .show()
         }
     }
 
-    suspend fun  downloadUserData() {
-        for (i in 1..200000) {
-            withContext(Dispatchers.Main){
-                tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
-
-            }
-//            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
-        }
+    private suspend fun getStock1() : Int{
+        delay(10000)
+        Log.i("LinLi", "stock 1 returned ");
+        return 55000
+    }
+    private suspend fun getStock2() : Int{
+        delay(8000)
+        Log.i("LinLi", "stock 2 returned ");
+        return 55000
     }
 }
